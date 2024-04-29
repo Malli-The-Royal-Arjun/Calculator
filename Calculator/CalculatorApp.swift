c963f3d461c49ff4ffb5fd4a8a8224f79dda5f20
Malli-The-Royal-Arjun <mupparaju.mallikarjuna007@gmail.com>
2024-04-29 15:15:18 +0530

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Arjun on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
