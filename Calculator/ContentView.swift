4c923be8fe256a29b2fa9bce1fc18e75a58a5b91
Malli-The-Royal-Arjun <mupparaju.mallikarjuna007@gmail.com>
2024-04-29 16:16:04 +0530

    Calculator App
    Similar Apple Calculator with SwiftUI

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -8,19 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject private var viewModel: CalculatorViewModel
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            displayText
+            buttonPad
         }
-        .padding()
+        .padding(Constants.padding)
+        .background(Color.black)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(CalculatorViewModel())
+    }
+}
+
+extension ContentView {
+    
+    private var displayText: some View {
+        Text(viewModel.displayText)
+            .padding()
+            .foregroundColor(.white)
+            .frame(maxWidth: .infinity, alignment: .trailing)
+            .font(.system(size: 88, weight: .light))
+            .lineLimit(1)
+            .minimumScaleFactor(0.2)
+    }
+    
+    private var buttonPad: some View {
+        VStack(spacing: Constants.padding) {
+            ForEach(viewModel.buttonTypes, id: \.self) { row in
+                HStack(spacing: Constants.padding) {
+                    ForEach(row, id: \.self) { buttonType in
+                        CalculatorButton(buttonType: buttonType)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct CalculatorButton: View {
+    
+    let buttonType: ButtonType
+    @EnvironmentObject private var viewModel: CalculatorViewModel
+    
+    var body: some View {
+        Button(buttonType.description) {
+            viewModel.performAction(for: buttonType)
+        }
+            .buttonStyle(CalculatorButtonStyle(
+                size: getButtonSize(),
+                backgroundColor: getBackgroundColor(),
+                foregroundColor: getForegroundColor(),
+                isWide: buttonType == .digit(.zero))
+            )
+    }
+    
+    private func getButtonSize() -> CGFloat {
+        let screenWidth = UIScreen.main.bounds.width
+        let buttonCount: CGFloat = 4
+        let spacingCount = buttonCount + 1
+        return (screenWidth - (spacingCount * Constants.padding)) / buttonCount
+    }
+    
+    private func getBackgroundColor() -> Color {
+        return viewModel.buttonTypeIsHighlighted(buttonType: buttonType) ? buttonType.foregroundColor : buttonType.backgroundColor
+    }
+    
+    private func getForegroundColor() -> Color {
+        return viewModel.buttonTypeIsHighlighted(buttonType: buttonType) ? buttonType.backgroundColor : buttonType.foregroundColor
     }
 }
---
+++ Calculator/Models.swift
@@ -0,0 +1,86 @@
+//
+//  Models.swift
+//  Calculator
+//
+//  Created by Arjun on 29/04/24.
+//
+
+import Foundation
+import SwiftUI
+
+enum ButtonType: Hashable, CustomStringConvertible {
+    case digit(_ digit: Digit)
+    case operation(_ operation: ArithmeticOperation)
+    case negative
+    case percent
+    case decimal
+    case equals
+    case allClear
+    case clear
+    
+    var description: String {
+        switch self {
+        case .digit(let digit):
+            return digit.description
+        case .operation(let operation):
+            return operation.description
+        case .negative:
+            return "±"
+        case .percent:
+            return "%"
+        case .decimal:
+            return "."
+        case .equals:
+            return "="
+        case .allClear:
+            return "AC"
+        case .clear:
+            return "C"
+        }
+    }
+    
+    var backgroundColor: Color {
+        switch self {
+        case .allClear, .clear, .negative, .percent:
+            return Color(.lightGray)
+        case .operation, .equals:
+            return .orange
+        case .digit, .decimal:
+            return .secondary
+        }
+    }
+    
+    var foregroundColor: Color {
+        switch self {
+        case .allClear, .clear, .negative, .percent:
+            return .black
+        default:
+            return .white
+        }
+    }
+}
+
+enum Digit: Int, CaseIterable, CustomStringConvertible {
+    case zero, one, two, three, four, five, six, seven, eight, nine
+    
+    var description: String {
+        "\(rawValue)"
+    }
+}
+
+enum ArithmeticOperation: CaseIterable, CustomStringConvertible {
+    case addition, subtraction, multiplication, division
+    
+    var description: String {
+        switch self {
+        case .addition:
+            return "+"
+        case .subtraction:
+            return "−"
+        case .multiplication:
+            return "×"
+        case .division:
+            return "÷"
+        }
+    }
+}
